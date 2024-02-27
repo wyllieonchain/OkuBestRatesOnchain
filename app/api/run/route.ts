83b16315e83cebc920d4492f2e7a9dcfd125eb75
Malcolm Wyllie <main@Malcolms-MacBook-Air.local>
2024-02-27 18:10:27 -0500

    fixing bug and new image

--- app/api/run/route.ts
+++ app/api/run/route.ts
@@ -23,7 +23,7 @@ const id = data.untrustedData.buttonIndex;*/
     <meta property="fc:frame" content="vNext" />
     <meta property="fc:frame:image" content="${process.env.NEXT_PUBLIC_BASE_URL}/chains.png" />
     <meta property="fc:frame:button:1" content="More Info" />
-    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end?${nextid}" />
+    <meta property="fc:frame:post_url" content="${process.env.NEXT_PUBLIC_BASE_URL}/api/end?id=${nextid}" />
   </head></html>`);
   } else if(idAsNumber === 2) {
   return new NextResponse(`<!DOCTYPE html><html><head>
